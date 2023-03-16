0469a85047e5fe21f56ef2996180f09c26e298e2
Junseong <david991129@naver.com>
2023-03-16 17:48:45 +0900

    스테틱 메서드 예제

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿public class MyClass
+{
+    private int val = 1;
+    public int InstRun()
+    {
+        return val;
+    }
+
+    public static int Run()
+    {
+        return 1;
+    }
+}
+
+public class Client
+{
+        public void Test()
+        {
+            MyClass myClass = new MyClass();
+            int i = myClass.InstRun();
+        }
+
+        public static void Testtwo()
+        {
+            int e = MyClass.Run();
+        }
+}
+
